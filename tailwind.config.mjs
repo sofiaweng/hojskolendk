e5ad18817dbee7088243ffa188af6f15b08c2182
Sofia Weng <sowe0001@stud.kea.dk>
2024-11-25 18:46:18 +0100

    fiks

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,87 +1,82 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 
-    /* Vil du overskrive eller extende tailwinds indstillinger?  */
+  /* Vil du overskrive eller extende tailwinds indstillinger?  */
 
-	theme: {
+  theme: {
+    /* Tilføj din egen farvepalette nedenfor */
+    orange: {
+      100: "#FFDECC",
+      200: "#FFBD99",
+      300: "#FF9C66",
+      400: "#FF7A33",
+      500: "#FF5900",
+      600: "#CC4700",
+      700: "#993600",
+      800: "#662400",
+      900: "#331200",
+    },
+    yellow: {
+      100: "#FDFFD6",
+      200: "#FBFFB3",
+      300: "#hexkode",
+      400: "#hexkode",
+      500: "#F3FE40",
+      600: "#hexkode",
+      700: "#hexkode",
+      800: "#hexkode",
+      900: "#hexkode",
+    },
+    white: "#fff",
+    black: "#000",
 
-        /* Tilføj din egen farvepalette nedenfor */
-        orange: {
-            100: "#FFDECC",
-            200: "#FFBD99",
-            300: "#FF9C66",
-            400: "#FF7A33",
-            500: "#FF5900",
-            600: "#CC4700",
-            700: "#993600",
-            800: "#662400",
-            900: "#331200",
-          },
-          yellow: {
-            100: "#FDFFD6",
-            200: "#FBFFB3",
-            300: "#hexkode",
-            400: "#hexkode",
-            500: "#F3FE40",
-            600: "#hexkode",
-            700: "#hexkode",
-            800: "#hexkode",
-            900: "#hexkode",
-          },
-          white: "#fff",
-          black: "#000",
-        },
-    
+    /* Tilføj din egen spacing nedenfor */
+    // spacing: {
+    //     'm': '40px',
+    // },
 
+    /* Tilføj fonte herunder - husk også at tilføje webfonte i head-sektionen i MainLayout  */
+    // fontFamily: {
+    //     sans: ['Prompt', 'sans-serif'],
+    //     'display': ['Courier', 'monospace'], /* eksempel på ekstra font */
+    // },
 
-         /* Tilføj din egen spacing nedenfor */
-        // spacing: {
-        //     'm': '40px',
-        // },
+    /* Tilføj dit typografiske hierarki herunder */
+    // fontSize: {
+    //     base: ['1rem', { lineHeight: '1.5rem' }],
+    //     large: ['2.5rem', { lineHeight: '3.75rem' }],
+    //   },
 
-        /* Tilføj fonte herunder - husk også at tilføje webfonte i head-sektionen i MainLayout  */
-        // fontFamily: {
-        //     sans: ['Prompt', 'sans-serif'],
-        //     'display': ['Courier', 'monospace'], /* eksempel på ekstra font */
-        // },
+    /* Tilføj dit fontvægt-hierarki nedenfor */
+    //   fontWeight: {
+    //     regular: '400',
+    //   },
 
-        /* Tilføj dit typografiske hierarki herunder */
-        // fontSize: {
-        //     base: ['1rem', { lineHeight: '1.5rem' }],
-        //     large: ['2.5rem', { lineHeight: '3.75rem' }],
-        //   },
+    /* Borders - borderstørrelser herunder */
+    // borderWidth: {
+    //     DEFAULT: '1px',
+    //     0: '0px',
+    //     2: '2px',
+    // },
 
-        /* Tilføj dit fontvægt-hierarki nedenfor */
-        //   fontWeight: {
-        //     regular: '400',
-        //   },
+    /* Border radius størrelser herunder */
+    // borderRadius: {
+    //     DEFAULT: '1.25rem',
+    //     none: '0px',
+    // },
 
-        /* Borders - borderstørrelser herunder */
-        // borderWidth: {
-        //     DEFAULT: '1px',
-        //     0: '0px',
-        //     2: '2px',
-        // },
+    /* Box shadows herunder */
+    // boxShadow: {
+    //     drop25: '4px 4px 4px 0 rgb(0 0 0 / 0.25)', /* x y blur spread farve / opacity */
+    // },
 
-        /* Border radius størrelser herunder */
-        // borderRadius: {
-        //     DEFAULT: '1.25rem',
-        //     none: '0px',
-        // },
-
-        /* Box shadows herunder */
-        // boxShadow: {
-        //     drop25: '4px 4px 4px 0 rgb(0 0 0 / 0.25)', /* x y blur spread farve / opacity */  
-        // },
-
-		extend: {
-        },
-	},
-	plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-    ],
-}
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/container-queries"),
+  ],
+};
